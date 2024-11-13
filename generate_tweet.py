0a978422bdd0934deb6004f8a667011168d6a2a9
jbt-02 <112265100+jbt-02@users.noreply.github.com>
2024-11-13 17:39:43 -0500

    Sets values for all keys in tweet_object other than the tweet itself

--- generate_tweet.py
+++ generate_tweet.py
@@ -2,6 +2,9 @@ import argparse
 from transformers import pipeline
 import torch
 import random
+import csv
+import json
+import uuid
 
 model_id = "meta-llama/Llama-3.2-3B-Instruct"
 
@@ -38,13 +41,14 @@ tweet_object = {
     "id-str": "", #tweet_id_str
     "text": "", #tweet
     "user": {
-        "id": 0 #user_id
-        "id-str": "" #user_id_str
+        "id": 0, #user_id
+        "id-str": "", #user_id_str
         "name": "", #screen_name
         "screen_name": "" #screen_name
     }
 }
 
+'''
 created_at = "" #GENERATE FROM AI MODEL
 tweet_id = 0
 tweet_id_str = ""
@@ -52,24 +56,45 @@ tweet = "" #GENERATE FROM AI MODEL
 type_tweet = args.threat_type
 user_id = 0
 user_id_str = ""
-name= "" #GENERATE FROM AI MODEL
+name="" #GENERATE FROM AI MODEL
 screen_name = "" #name without spaces
+'''
 
-if(args.time_or_single == "single"):
-    i = 0
-    while i < args.number_tweets:
-        tweet_id = random_number = random.randint(0, 10000000)
-        tweet_id_str = str(tweet_id)
-        #Generate created_at and Tweet in here for each tweet
-
-
-#This is how you generate stuff
 pipe = pipeline(
     "text-generation",
     model=model_id,
     device="cpu",  # Forces CPU usage
     torch_dtype=torch.float16,  # float16 is more compatible across devices
 )
+
+if(args.time_or_single == "single"):
+    i = 0
+    while i < args.number_tweets:
+        tweet_object["tweet_id"] = uuid.uuid4()
+        tweet_object["user"]["user_id"] = uuid.uuid4()
+        tweet_object["tweet_id_str"] = str(tweet_object["tweet_id"])
+        tweet_object["user"]["user_id_str"] = str(tweet_object["user"]["user_id"])
+
+        #Generate created_at, Tweet, and username in here for each tweet
+        prompt = f"only generate a created_at value for a tweet object"
+        tweet_object["created_at"] = pipe(prompt, max_new_tokens=50)
+
+        prompt = f"only generate a username for a twitter account"
+        tweet_object["user"]["name"] = pipe(prompt, max_new_tokens=20)
+
+        if(" " in tweet_object["name"]):
+            tweet_object["user"]["screen_name"] = tweet_object["user"]["name"].replace(" ", "")
+        else:
+            tweet_object["user"]["screen_name"] = tweet_object["user"]["name"]
+
+        prompt = f""
+        tweet = pipe(prompt, max_new_tokens=185)
+
+        #APPEND DATA TO CSV FILE DOWN HERE
+
+        
+
+
 
 prompt = f"Write a tweet that impersonates a {args.threat_type} insider threat."
 response = pipe(prompt, max_new_tokens=185)
