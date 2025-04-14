fa3eb3a9dc971c2e527bef7fc78b0e4eb46c7ddc
jbt-02 <112265100+jbt-02@users.noreply.github.com>
2025-04-14 10:39:34 -0400

    Only allows generation when all fields set

--- gui/main.gui.py
+++ gui/main.gui.py
@@ -11,6 +11,18 @@ def browse_file():
         dest_input.delete(0, tk.END) 
         dest_input.insert(0, filename) 
 
+
+def check_all_inputs():
+    if dest_input.get().strip() == "" or num_tweets_input.get().strip() == "" or time_or_single_option.get() == "" or not listbox.curselection():
+        button.config(state=tk.DISABLED)
+        return
+    
+    button.config(state=tk.NORMAL)
+
+def on_listbox_select(event):
+    check_all_inputs()
+        
+
 def on_generate():
     # Collect user inputs from the GUI
     print("Generating Tweets")
@@ -18,7 +30,8 @@ def on_generate():
     num_tweets = int(num_tweets_input.get())
     generation_type = time_or_single_option.get()
     selected_threat_types = [listbox.get(i) for i in listbox.curselection()]
-    button.config(state="disabled", text="Generating Tweets...")
+
+    
 
     # Call the back-end function with the collected inputs
     if(generation_type == "Single"):
@@ -45,6 +58,7 @@ ttk.Label(root, text="Configure your tweet generation parameters", font=("Helvet
 ttk.Label(root, text="Destination").grid(row=2, column=0, pady=5, padx=10, sticky="w")
 dest_input = tk.Entry(root)
 dest_input.grid(row=3, column=0, pady=5, padx=10, sticky="we")
+dest_input.bind("<KeyRelease>", lambda e: check_all_inputs())
 
 browse_button = ttk.Button(root, text="Browse", command=browse_file)
 browse_button.grid(row=3, column=1)
@@ -53,14 +67,16 @@ browse_button.grid(row=3, column=1)
 ttk.Label(root, text="Number of Tweets").grid(row=4, column=0, pady=5, padx=10, sticky="w")
 num_tweets_input = ttk.Entry(root, width=30)
 num_tweets_input.grid(row=5, column=0, pady=5, padx=10, sticky="ew")
+num_tweets_input.bind("<KeyRelease>", lambda e: check_all_inputs())
 
 # Generation Type Radio Buttons
 time_or_single_option = tk.StringVar()
 time_or_single_option.set("Single")
 
+
 ttk.Label(root, text="Generation Type").grid(row=6, column=0, pady=5, padx=10, sticky="w")
-radio1 = ttk.Radiobutton(root, text="Single Tweet", variable=time_or_single_option, value="Single").grid(row=7, column=0, pady=5, padx=10, sticky="w")
-radio2 = ttk.Radiobutton(root, text="Time Series Tweet", variable=time_or_single_option, value="Time Series").grid(row=7, column=0, pady=5, padx=125, sticky="w")
+radio1 = ttk.Radiobutton(root, text="Single Tweet", variable=time_or_single_option, value="Single", command=check_all_inputs).grid(row=7, column=0, pady=5, padx=10, sticky="w")
+radio2 = ttk.Radiobutton(root, text="Time Series Tweet", variable=time_or_single_option, value="Time Series", command=check_all_inputs).grid(row=7, column=0, pady=5, padx=125, sticky="w")
 
 # Insider Threat Types Listbox
 ttk.Label(root, text="Insider Threat Types").grid(row=8, column=0, pady=5, padx=10, sticky="w")
@@ -68,9 +84,10 @@ listbox = tk.Listbox(root, selectmode=tk.MULTIPLE, height=3, width=30)
 for idx, element in enumerate(insider_threat_types):
     listbox.insert(idx, element)
 listbox.grid(row=9, column=0, padx=10, pady=5, sticky="w")
+listbox.bind("<<ListboxSelect>>", on_listbox_select)
 
 # Generate Button
-button = ttk.Button(root, text="Generate", width=82,  command=lambda: on_generate())
+button = ttk.Button(root, text="Generate", width=82, state=tk.DISABLED, command=lambda: (button.config(state=tk.DISABLED, text="Generating Tweets..."), on_generate()))
 button.grid(row=10, column=0, padx=10, pady=10, sticky="w")
 
 root.mainloop()
